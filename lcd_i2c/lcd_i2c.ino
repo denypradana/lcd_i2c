2b78ed33719d63b9fe1b4410fbf9a0b72a46b732
Deny Pradana <pradana.deny@gmail.com>
2019-05-14 10:43:35 +0700

    Added RTC

--- lcd_i2c/lcd_i2c.ino
+++ lcd_i2c/lcd_i2c.ino
@@ -1,13 +1,33 @@
 #include <LiquidCrystal_I2C.h>
+#include <DS3231.h>
+
+DS3231 rtc(SDA,SCL);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
  
 void setup() {
+  rtc.begin();
+  //rtc.setDate(11, 5, 2019);   //mensetting tanggal 11 mei 2019
+  //rtc.setTime(17, 37, 20);     //menset jam 17:37:20
+  //rtc.setDOW(6);     //menset hari "Sabtu"
   lcd.begin();
 }
- 
+
 void loop(){
-  lcd.setCursor(2,0);
-  lcd.print("TEST LCD i2C");
-  lcd.setCursor(2,1);
-  lcd.print("Deny Pradana");
+lcd.setCursor(0,0);
+lcd.print(rtc.getDOWStr(1)); //prosedur pemanggilan hari
+lcd.setCursor(3,0);
+lcd.print(",");
+ 
+lcd.setCursor(4,0);
+lcd.print(rtc.getDateStr()); //prosedur pemanggilan tanggal
+ 
+lcd.setCursor(4,1);
+lcd.print(rtc.getTimeStr()); //prosedur pemanggilan waktu
+
+//lcd.setCursor(10,1);
+//lcd.print("t:");
+ 
+//lcd.setCursor(12,1);
+//lcd.print(rtc.getTemp()); //prosedur pemanggilan suhu
 }
